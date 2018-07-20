4d94daf05554f82a891a5f309d0ce093d70418dd
tjansen17 <tnj@17cosmicln.net>
2018-07-20 11:21:58 -0400

    Pom standardized. Spring boot start up changed

--- src/main/java/com/revature/assignforce/CurriculumServiceApplication.java
+++ src/main/java/com/revature/assignforce/CurriculumServiceApplication.java
@@ -2,6 +2,7 @@ package com.revature.assignforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
@@ -9,6 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class CurriculumServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurriculumServiceApplication.class, args);
+		new SpringApplicationBuilder(CurriculumServiceApplication.class).run(args);
+
+//		SpringApplication.run(CurriculumServiceApplication.class, args);
 	}
 }
