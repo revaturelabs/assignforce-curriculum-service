b01f30a8e70332b6a83364588eb6ef01b473bdec
Karl Pasciak <pasciakarl@gmail.com>
2018-08-14 13:45:45 -0400

    added security [debug]

--- src/main/java/com/revature/assignforce/CurriculumServiceApplication.java
+++ src/main/java/com/revature/assignforce/CurriculumServiceApplication.java
@@ -3,9 +3,11 @@ package com.revature.assignforce;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebSecurity
 public class CurriculumServiceApplication {
 
 	public static void main(String[] args) {
