5c52775100033d9ca44a2c3e411f042aca8edbea
August Duet <august.duet@revature.com>
2018-12-04 13:58:21 -0500

    add listener for skill changes

---
+++ src/main/java/com/revature/assignforce/beans/SkillMessage.java
@@ -0,0 +1,26 @@
+package com.revature.assignforce.beans;
+
+public class SkillMessage {
+    private String context;
+    private Integer skillId;
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public Integer getSkillId() {
+        return skillId;
+    }
+
+    public void setSkillId(Integer skillId) {
+        this.skillId = skillId;
+    }
+
+    public SkillMessage() {
+
+    }
+}
