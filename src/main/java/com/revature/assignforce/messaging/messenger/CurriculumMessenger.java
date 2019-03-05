648e5f15f3b118d53c410ebc4717b5c470dfdded
August Duet <august.duet@revature.com>
2019-03-05 10:01:02 -0500

    setting up for new pipeline and deployment

--- src/main/java/com/revature/assignforce/messaging/messenger/CurriculumMessenger.java
+++ src/main/java/com/revature/assignforce/messaging/messenger/CurriculumMessenger.java
@@ -2,7 +2,6 @@ package com.revature.assignforce.messaging.messenger;
 
 import javax.inject.Inject;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,23 +9,13 @@ import com.revature.assignforce.beans.Curriculum;
 
 @Component
 public class CurriculumMessenger {
-
-	private final RabbitTemplate rabbitTemplate;
 	
-	@Value("${spring.rabbitmq.exchange:assignforce}")
 	private String exchange;
 	
-	@Value("${spring.rabbitmq.curriculum-routing-delete:assignforce.curriculum.delete")
 	private String routingKey;
-	
-	@Inject
-	public CurriculumMessenger(RabbitTemplate rabbitTemplate) {
-		super();
-		this.rabbitTemplate = rabbitTemplate;
-	}
+
 	
 	public void sendDeletionMessage(int id) {
-		rabbitTemplate.convertAndSend(exchange, routingKey, id);
 	}
 	
 }
