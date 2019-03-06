7271fe8455fec241b50498c07cfa45db71a86f0e
August Duet <august.duet@revature.com>
2019-03-06 14:44:46 -0500

    fixing bad request on post

--- src/main/java/com/revature/assignforce/config/BeansConfig.java
+++ src/main/java/com/revature/assignforce/config/BeansConfig.java
@@ -1,5 +1,7 @@
 package com.revature.assignforce.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.assignforce.service.CurriculumService;
 import com.revature.assignforce.service.CurriculumServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +12,10 @@ public class BeansConfig {
     @Bean
     public CurriculumService curriculumService() {
         return new CurriculumServiceImpl();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 }
